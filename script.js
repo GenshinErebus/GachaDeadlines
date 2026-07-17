e45b3fdd01b59c4ab4f69d0b11beeeacdc557879
GenshinErebus <critic1985@googlemail.com>
2026-07-17 11:38:41 +0200

    updated ZZZ events

--- script.js
+++ script.js
@@ -116,17 +116,12 @@ const eventDatabase = {
             {
                 id: "zzz_006",
                 name: "The Final Callback",
-                endDate: "2026-07-27T03:00:00Z"
+                endDate: "2026-07-28T03:00:00Z"
             },
             {
                 id: "zzz_007",
                 name: "Deadly Assault",
-                endDate: "2026-07-17T03:00:00Z"
-            },
-            {
-                id: "zzz_00",
-                name: "Return to Ridu: Together in a New Chapter",
-                endDate: "2026-07-17T03:00:00Z"
+                endDate: "2026-07-28T03:00:00Z"
             }
         ]
     },
