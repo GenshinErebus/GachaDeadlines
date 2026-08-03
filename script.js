bb64721f9b2341f0cc9768b02254c7b7424a99d5
GenshinErebus <critic1985@googlemail.com>
2026-08-03 18:51:41 +0200

    test14

--- script.js
+++ script.js
@@ -286,6 +286,7 @@ const eventDatabase = {
     duet: {
         name: "Duet Night Abyss",
         events: [
+            // General Events (10 total)
             {
                 id: "dna_001",
                 name: "Paradise Prelude",
